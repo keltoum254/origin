46fe392e8d48f080384ecd7bb26c1180f12d3af2
keltoum254 <baddikeltoum511@gmail.com>
2024-01-12 23:39:54 +0100

    Update production.py

--- firstproject/settings/production.py
+++ firstproject/settings/production.py
@@ -154,6 +154,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'media')
 # 'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
 # },
 # }
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
